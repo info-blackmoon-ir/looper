c8accc76955ccf517ed2f1cf229baa6b3246478f
Mahdi-rzei <64587931+Mahdi-rzei@users.noreply.github.com>
2020-12-30 17:28:04 +0330

    lol
    lol

---
+++ Assets/_Scripts/Environments/LooperDesign.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LooperDesign : MonoBehaviour
+{
+    [SerializeField] private GameObject looper;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Instantiate( Transform transform)
+    {
+
+
+        Instantiate(looper, transform.position, transform.rotation);
+    }
+}
---
+++ Assets/_Scripts/Environments/LooperTransparent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LooperTransparent : MonoBehaviour
+{
+    private LooperDesign theLooperDesign;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theLooperDesign = GameObject.FindObjectOfType<LooperDesign>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        //if (col.getcomponent<looperdesign>())
+        //{
+        //    this.gameobject.setactive(false);
+        //}
+        print("olde");
+    }
+
+    private void OnMouseDown()
+    {
+        theLooperDesign.Instantiate(this.transform);
+
+    }
+
+}
