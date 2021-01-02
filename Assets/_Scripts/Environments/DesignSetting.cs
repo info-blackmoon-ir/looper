bcac8c70ec4bffc563f93d1af1fdd32ef178d3ca
Mahdi-rzei <64587931+Mahdi-rzei@users.noreply.github.com>
2021-01-02 12:46:10 +0330

    Auto generating modefied
    new cool modification that you probably won't notice.
    It's like clearing under the bed. You won't notice the change, but we will.

--- Assets/_Scripts/Environments/DesignSetting.cs
+++ Assets/_Scripts/Environments/DesignSetting.cs
@@ -9,7 +9,6 @@ public class DesignSetting : MonoBehaviour
     public LooperTransparent[] allTransparentChilds;
     public List<GameObject> Loopers = new List<GameObject>();
     public int looperAmount = 9;
-    public bool isAutoGenerate = false;
 
     [SerializeField] private TextMeshProUGUI looperAmontTxt;
 
@@ -22,22 +21,7 @@ public class DesignSetting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        allTransparentChilds = GameObject.FindObjectsOfType<LooperTransparent>();
-    }
-
-    // Deactivates the transparent guide children,
-    // and brings up the color panel.
-    public void FinalizeDesign()
-    {
-        for (int i = 0; i < Loopers.Count; i++)
-        {
-            foreach (GameObject looper in Loopers)
-            {
-                looper.GetComponent<LooperDesign>().DeactivateChildren();
-            }
-        }
-
-        // TODO: Switch the Amount panel with color panel.
+        
     }
 
     // Increases the amount of Loopers used for auto generating.
@@ -68,9 +52,17 @@ public class DesignSetting : MonoBehaviour
         looperAmontTxt.text = looperAmount.ToString();
     }
 
+    // Triggers the Auto generation
+    public void TriggerAutoGeneration()
+    {
+        Clear();
+
+        StartCoroutine(AutoGenerateCoroutine());
+    }
+
     // Destroys any pre-existing Loopers and clears the list,
-    // and spawns a new looper with auto generating On.
-    public void AutoGenerateLoopers()
+    // and spawns a new Looper.
+    public void Clear()
     {
         foreach (GameObject looper in Loopers)
         {
@@ -79,8 +71,55 @@ public class DesignSetting : MonoBehaviour
 
         Loopers.Clear();
 
-        isAutoGenerate = true;
-
         Instantiate(looper, transform.position, Quaternion.Euler(0, 180, 0));
     }
+
+    // Starts to aoutomatically spawn Loopers.
+    public IEnumerator AutoGenerateCoroutine()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        
+
+        for (int i = 1; i < looperAmount; i++)
+        {
+            allTransparentChilds = GameObject.FindObjectsOfType<LooperTransparent>();
+
+            int randomIndex = Random.Range(0, allTransparentChilds.Length);
+
+            GameObject go = Instantiate(looper,
+                allTransparentChilds[randomIndex].transform.position,
+                Quaternion.Euler(0, allTransparentChilds[randomIndex].transform.eulerAngles.y + 180,
+                allTransparentChilds[randomIndex].transform.eulerAngles.z));
+
+            go.transform.rotation = Quaternion.Euler(0, 180, go.transform.rotation.eulerAngles.z);
+
+            yield return new WaitForSeconds(0.2f);
+
+            if (i == looperAmount - 1)  
+            {
+                FinalizeDesign();
+            }
+        }
+
+    }
+
+    // Deactivates the transparent guide children,
+    public void FinalizeDesign()
+    {
+        allTransparentChilds = GameObject.FindObjectsOfType<LooperTransparent>();
+
+        foreach (LooperTransparent child in allTransparentChilds)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+    }
+
+    // TODO: Switch the Amount panel with color panel.
+    public void SetUpColoringPanel()
+    {
+
+    }
+    
 }
--- Assets/_Scripts/Environments/LooperDesign.cs
+++ Assets/_Scripts/Environments/LooperDesign.cs
@@ -19,51 +19,11 @@ public class LooperDesign : MonoBehaviour
             myTransparentChilds[i].SetActive(true);
         }
 
-        if (theDesignSetting.Loopers.Count <= theDesignSetting.looperAmount && theDesignSetting.isAutoGenerate)
-        {
-            StartCoroutine(AutoSpawnCoroutine());
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    // Deactivates all its child transparent Loopers/
-    public void DeactivateChildren()
-    {
-        for (int i = 0; i < myTransparentChilds.Length; i++)
-        {
-            myTransparentChilds[i].SetActive(false);
-        }
-    }
-
-    // Each Looper Spawns the next one in a random position
-    private IEnumerator AutoSpawnCoroutine()
-    { 
-        yield return new WaitForSeconds(0.1f);
-
-        if (theDesignSetting.Loopers.Count == theDesignSetting.looperAmount)
-        {
-            theDesignSetting.isAutoGenerate = false;
-            theDesignSetting.FinalizeDesign();
-        }
-        else
-        {
-            int randomIndex = Random.Range(0, theDesignSetting.allTransparentChilds.Length);
-
-            GameObject go = Instantiate(theDesignSetting.looper,
-                theDesignSetting.allTransparentChilds[randomIndex].transform.position,
-                Quaternion.Euler(0, theDesignSetting.allTransparentChilds[randomIndex].transform.eulerAngles.y + 180,
-                theDesignSetting.allTransparentChilds[randomIndex].transform.eulerAngles.z));
-
-            go.transform.rotation = Quaternion.Euler(0, 180, go.transform.rotation.eulerAngles.z);
-        }
-
-
-
 
     }
 
