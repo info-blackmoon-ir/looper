8412b12659cab8da9efedd80feae2a190532ec36
Mahdi-rzei <64587931+Mahdi-rzei@users.noreply.github.com>
2021-01-18 16:24:07 +0330

    color picker
    new round color picker added

---
+++ Assets/ColorPicker.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPicker : MonoBehaviour
+{
+    [SerializeField] private GameObject[] colorsList;
+
+    [SerializeField] float rotationSpeed = 100f;
+    private bool isDragging = false;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        isDragging = true;
+        
+    }
+
+    private void OnMouseDown()
+    {
+        rb.freezeRotation = true;
+        rb.freezeRotation = false;
+        print("true");
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDragging)
+        {
+            float x = Input.GetAxis("Mouse X") * rotationSpeed * Time.fixedDeltaTime;
+            float y = Input.GetAxis("Mouse Y") * rotationSpeed * Time.fixedDeltaTime;
+
+
+            rb.AddTorque(Vector3.forward * -x);
+            rb.AddTorque(Vector3.forward * y);
+        }
+    }
+}
+
---
+++ Assets/Colors.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colors : MonoBehaviour
+{
+    [SerializeField] private string btnHexCode;
+    public Color collli;
+
+    private DesignSetting theDesignSetting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theDesignSetting = GameObject.FindObjectOfType<DesignSetting>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        theDesignSetting.looperHexColor = btnHexCode;
+    }
+}
